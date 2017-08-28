39b0423c6d83bdbd3ebc65f6b96f48dd83e0f5bb
xieyang <xieyang@memect.co>
2017-08-28 17:00:43 +0800

    [feature]add estimator and pipeline

--- chapter1/category.py
+++ chapter1/category.py
@@ -1,10 +1,54 @@
+#-*- coding: UTF-8 -*-
+
+import numpy as np
 from sklearn.datasets import load_iris
+from collections import defaultdict
+from operator import itemgetter
+
+from sklearn.cross_validation import train_test_split
+
 dataset = load_iris()
 
 features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+plants = ['Iris Setosa', 'Iris Versicolour', 'Iris Virginica']
 
 X = dataset.data
 Y = dataset.target
 
-print(dataset.DESCR)
-print(Y)
+attribute_means = X.mean(axis=0)
+
+x_d = np.array(X >= attribute_means, dtype="int")
+
+Xd_train, Xd_test, y_train, y_test = train_test_split(x_d, Y, random_state=14)
+
+print(Xd_train, Xd_test, y_train, y_test)
+
+def train_feature_value(X, y_true, feature_index, value):
+    class_counts = defaultdict(int)
+    for sample, y in zip(X, y_true):
+        if sample[feature_index] == value:
+            class_counts[y] += 1
+
+    sorted_class_counts = sorted(class_counts.items(), key=itemgetter(1), reverse=True)
+    most_frequent_class = sorted_class_counts[0][0]
+
+    incorrect_predictions = [class_count for class_value, class_count in class_counts.items() if class_value != most_frequent_class]
+    error = sum(incorrect_predictions)
+
+    return most_frequent_class, error
+
+def train_on_feature(X, y_true, feature_index):
+    values = set(X[:, feature_index])
+    predictors = {}
+    errors = []
+
+    for current_value in values:
+        most_frequent_class, error = train_feature_value(X, y_true, feature_index, current_value)
+        predictors[current_value] = most_frequent_class
+        errors.append(error)
+
+    total_error = sum(errors)
+    return predictors, total_error
+
+
+
---
+++ chapter2/estimator.py
@@ -0,0 +1,67 @@
+#-*- coding: UTF-8 -*-
+
+import numpy as np 
+import csv
+import os
+
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+
+data_filder = './data'
+
+data_filename = os.path.join(data_filder, '', 'ionosphere.data')
+
+x = np.zeros((351, 34), dtype='float')
+y = np.zeros((351,), dtype="bool")
+
+with open(data_filename, 'r') as input_file:
+    reader = csv.reader(input_file)
+
+    for i, row in enumerate(reader):
+        data = [float(dataum) for dataum in row[:-1]]
+        x[i] = data
+
+        y[i] = row[-1] == 'g'
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=14)
+
+estimator = KNeighborsClassifier()
+estimator.fit(x_train, y_train)
+
+y_predicted = estimator.predict(x_test)
+accuracy = np.mean(y_test == y_predicted) * 100 #平均值
+print(accuracy)
+
+scores = cross_val_score(estimator, x, y, scoring="accuracy")
+average_accuracy = np.mean(scores) * 100
+print(average_accuracy)
+
+avg_scores = []
+all_scores = []
+
+parameter_values = list(range(1, 21)) #include 20
+
+x_broken = np.array(x)
+
+x_broken[:,::2] /= 10
+
+for n_neighbors in parameter_values:
+    estimator = KNeighborsClassifier(n_neighbors=n_neighbors)
+    scores = cross_val_score(estimator, x, y, scoring='accuracy')
+    avg_scores.append(np.mean(scores))
+    all_scores.append(scores)
+
+# print(avg_scores)
+# print(all_scores)
+
+broken_scores = np.mean(cross_val_score(estimator, x, y, scoring='accuracy'))
+print(broken_scores * 100)
+
+x_transformed = MinMaxScaler().fit_transform(x_broken)
+estimator = KNeighborsClassifier()
+transformed_scores = cross_val_score(estimator, x_transformed, y, scoring='accuracy')
+print(np.mean(transformed_scores) * 100)
+
+
